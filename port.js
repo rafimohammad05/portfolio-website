f3e35f6ad577f3483383e028bc988ff5ba8e4fd1
rafimohammad05 <rafimohammadrafi055@gmail.com>
2025-10-25 12:44:13 +0530

    Create port.js

---
+++ port.js
@@ -0,0 +1,10 @@
+<script>
+  // Show a welcome prompt when the site opens
+  let name = prompt("Welcome! Rafi Mohammad");
+  
+  if (name) {
+    alert("Hello " + Rafi + "! 👋 Welcome to my portfolio website.");
+  } else {
+    alert("Welcome to my portfolio website!");
+  }
+</script>
